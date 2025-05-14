7f24e55676525786e3a92edfedb9869b9cc31bfd
EstebanRojas521 <71135521+EstebanRojas521@users.noreply.github.com>
2025-05-14 13:44:32 -0600

    feat(posts): add delete own post functionality
    - Implemented delete own post service with user validation.
    - Created delete own post controller with error handling for missing postId, unauthorized user, and already deleted post.
    - Set up route for delete own post with JWT authentication middleware.
    - Added tests for service, controller, and route.
    - Used Jest to mock services and middleware, ensuring isolated tests without database interactions.

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response, NextFunction } from 'express';
 import { errorHandler } from './utils/errors/error-handler.middleware';
 import cors from "cors";
+import postRoutes from './features/posts/routes/post.routes'; // Adjust the path if your routes file is elsewhere
 
 export const app = express();
 const PORT = 3000;
@@ -10,7 +11,7 @@ app.get('/', (req, res) => {
 });
 app.use(express.json());
 app.use(cors());
-
+app.use('/api', postRoutes);
 // Error handling middleware should be last
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     errorHandler(err, req, res, next);
