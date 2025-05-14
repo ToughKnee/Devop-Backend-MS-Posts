698aaa09827991723ae28f60316bb609ffcda0b0
AngelChaves <angel.chaveschinchilla@ucr.ac.cr>
2025-05-13 22:19:16 -0600

    feat: update BasePost interface to make content, file_url, and media_type optional

--- src/features/posts/interfaces/posts.entities.interface.ts
+++ src/features/posts/interfaces/posts.entities.interface.ts
@@ -1,8 +1,8 @@
 export interface BasePost {
   id: string;
-  content: string; // could be null, but it is a required field, max length 300
-  file_url: string; // Could be null, but it is a required field, it is a URL to the file (stored in UploadThing) 
-  media_type: number; // in bytes, max size 5MB
+  content?: string; // could be null, but it is a required field, max length 300
+  file_url?: string; // Could be null, but it is a required field, it is a URL to the file (stored in UploadThing) 
+  media_type?: number; // in bytes, max size 5MB
   created_at: Date;
 }
 
